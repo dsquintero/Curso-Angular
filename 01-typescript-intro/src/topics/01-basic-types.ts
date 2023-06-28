1d738bde380cca23fe0dc66ac49ab98e7ae96b72
Daniel Quintero <dquintero.ing@gmail.com>
2023-06-27 22:48:33 -0500

    ts

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: string = 'Strider';
+let hdPoints: number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+hdPoints = 'FULL';
+
+console.log({
+    name, hdPoints, isAlive
+});
+
+export {};
